30a41c4526ddd1a253c509cc932d55e6875c2076
Documenter.jl <documenter@juliadocs.github.io>
2023-06-21 17:02:00 +0000

    build based on e77db8c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RepLieGroups","category":"page"},{"location":"#RepLieGroups","page":"Home","title":"RepLieGroups","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RepLieGroups.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RepLieGroups]","category":"page"}]
+}
