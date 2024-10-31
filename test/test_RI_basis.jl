64d8b4495f46eb3163909c7c6af29d19fdaa9dd4
dussong <genevieve.dusson@univ-fcomte.fr>
2024-10-31 10:55:12 +0100

    test RPI - debug

--- test/test_RI_basis.jl
+++ test/test_RI_basis.jl
@@ -57,14 +57,14 @@ end
 # for the moment the code with generalized CG only works with L=0
 L = 0
 cc = Rot3DCoeffs(L)
-ll = SA[2,2,2,3,3]
+ll = SA[1,1,2,2,4]
 
 # version with svd
 @time coeffs1, MM1 = O3.re_basis(cc, ll)
 nbas = size(coeffs1, 1)
 
 #version with gen CG coefficients
-@time coeffs2, MM2 = re_basis_new(ll)
+@time coeffs2, MM2 = ri_basis_new(ll)
 
 # simple test on size
 @test size(coeffs1) == size(coeffs2)
@@ -85,7 +85,7 @@ coeffsp2 = coeffs2[:,P2]
 @test rank(coeffsp2) == size(coeffsp2,1)
 
 # check that the coef span the same space - test fails
-@test nbas == rank([coeffsp; coeffsp2], rtol = 1e-12)
+@test nbas == rank([coeffsp1; coeffsp2], rtol = 1e-12)
 
 
 Rs = [rand_sphere() for _ in 1:length(ll)]
@@ -123,3 +123,61 @@ end
 # check that functions span same space
 rk = rank([A1;A2]; rtol = 1e-12)  
 @test rk == nbas
+
+# ----------------------------
+# Extension to equivariance 
+# ----------------------------
+
+# Test SetLl0
+N = 5
+l = rand(1:10, N)
+SL1 = RepLieGroups.SetLl(l,N,0)
+SL2 = RepLieGroups.SetLl0(l,N)
+@test SL1 == SL2
+
+# Test SetML 
+N = 5
+l = rand(1:10, N)
+SM1 = RepLieGroups.ML(l,N,0)
+SM2 = RepLieGroups.ML0(l,N)
+@test SM1 == SM2
+
+ 
+# # Version CO
+# L = 1
+# cc = Rot3DCoeffs(L)
+# ll = SA[1,1,2,3,4]
+
+# # version with svd
+# @time coeffs1, MM1 = O3.re_basis(cc, ll)
+# nbas = size(coeffs1, 1)
+
+# # Version GD
+# @time coeffs2, MM2 = re_basis_new(ll,L)
+
+# # simple test on size
+# @test size(coeffs1) == size(coeffs2)
+# @test size(MM1) == size(MM2)
+
+# P1 = sortperm(MM1)
+# P2 = sortperm(MM2)
+# MMsorted1 = MM1[P1]
+# MMsorted2 = MM2[P2]
+# # check that same mm values
+# @test MMsorted1 == MMsorted2
+
+# coeffsp1 = coeffs1[:,P1]
+# coeffsp2 = coeffs2[:,P2]
+
+# # test that full rank
+# @test rank(coeffsp1) == size(coeffsp1,1)
+# @test rank(coeffsp2) == size(coeffsp2,1)
+
+# # check that the coef span the same space 
+# @test nbas == rank([coeffsp1; coeffsp2], rtol = 1e-12)
+
+# Rs = [rand_sphere() for _ in 1:length(ll)]
+# Q = rand_rot() 
+# QRs = [Q*Rs[i] for i in 1:length(Rs)]
+# fRs1 = [ f(Rs, q; coeffs=coeffs1, MM=MM1, ll=ll) for q = 1:nbas ]
+# fRs1Q = [ f(QRs, q; coeffs=coeffs1, MM=MM1, ll=ll) for q = 1:nbas ]
--- test/test_RPI_basis.jl
+++ test/test_RPI_basis.jl
@@ -1,9 +1,10 @@
 
 using SpheriCart, StaticArrays, LinearAlgebra, RepLieGroups, WignerD, 
       Combinatorics
-using Rotations
-using RepLieGroups.O3: Rot3DCoeffs
+using RepLieGroups.O3: Rot3DCoeffs, Rot3DCoeffs_real
 O3 = RepLieGroups.O3
+using Test
+
 
 function eval_cY(rbasis::SphericalHarmonics{LMAX}, 𝐫) where {LMAX}  
    Yr = rbasis(𝐫)
@@ -30,112 +31,75 @@ function rand_sphere()
    return u / norm(u) 
 end
 
+function rand_radial(rmax) 
+   return rmax*rand()
+end
+
 function rand_rot() 
    K = @SMatrix randn(3,3)
    return exp(K - K') 
 end
 
-
-##
-# CASE 2: 4-correlations, L = 0 (revisited)
-L = 0
-cc = Rot3DCoeffs(0)
-# now we fix an ll = (l1, l2, l3) triple ask for all possible linear combinations 
-# of the tensor product basis   Y[l1, m1] * Y[l2, m2] * Y[l3, m3] * Y[l4, m4]
-# that are invariant under O(3) rotations.
-ll = SA[3, 3, 2, 2, 2]
-coeffs, MM = O3.re_basis(cc, ll)
-nbas = size(coeffs, 1)
-# coeffs = nbasis x length(MM) matrix 
-#     MM = vector of (m1, m2, m3, m4) tuples 
-# for 4-correlations and higher, the number of possible couplings can be 
-# greater than one. An interesting result of this is that even if those 
-# basis functions as tensor products of Ylms are linearly independent, they 
-# need no longer be linearly independent once we impose permutation-invariance. 
-
 """
 This implements a permutation-invariant and O(3) invariant function of 
 4 (or more) variables. 
 """
-function f(Rs, q::Integer; coeffs=coeffs, MM=MM)
-   real_basis = SphericalHarmonics(3)
+function f(Rs, q::Integer; coeffs=coeffs, MM=MM, ll=ll)
+   N = length(coeffs[1])
+   Lmax = maximum(ll)
+   real_basis = SphericalHarmonics(Lmax)
    Y = [ eval_cY(real_basis, 𝐫) for 𝐫 in Rs ]
    A = sum(Y)   # this is a permutation-invariant embedding
    out = zero(eltype(A))
    for (c, mm) in zip(coeffs[q, :], MM)
-      ii = [SpheriCart.lm2idx(ll[α], mm[α]) for α in 1:4]
+      ii = [SpheriCart.lm2idx(ll[α], mm[α]) for α in 1:N]
       out += c * prod(A[ii])
    end
    return real(out)
 end
 
-
-Rs = [rand_sphere() for _ in 1:4]
-[ f(Rs, q) for q = 1:nbas ]
-
 # we can look for linear independence... 
-
-function f_rand_batch(coeffs, MM, ntest) 
+function f_rand_batch(coeffs, MM, ntest, ll) 
+   N = length(MM[1])
    nbas = size(coeffs, 1)
    A = zeros(nbas, ntest)
    for i = 1:ntest 
-      Rs = [rand_sphere() for _ in 1:4]
+      Rs = [rand_sphere() for _ in 1:N]
       for q = 1:nbas
-         A[q, i] = f(Rs, q; coeffs = coeffs)
+         A[q, i] = f(Rs, q; coeffs = coeffs, MM=MM, ll=ll)
       end
    end
    return A
 end
 
-A = f_rand_batch(coeffs, MM, 1_000)
-
-# the rank of this matrix is only 3, not nbas = 5!
-rk = rank(A)  
-# 3 
-
-# this is in fact very clear from the SVD 
-svdvals(A)
-# 5-element Vector{Float64}:
-#  25.568548451339442
-#   6.754493909270821
-#   5.45667827344765
-#   5.398138581058422e-15
-#   1.6731699590390224e-15
-
 ##
-# In ACE we use a semi-analytic construction to make the basis functions 
-# linearly indepdendent. 
-# in a full ACE code, this is a bit more complex since n channels are added 
-# to the story. This can be found here: 
-# https://github.com/ACEsuit/ACE1.jl/blob/8ac52d2128241a01d8b9a036f41b1d5106cbeb07/src/rpi/rotations3d.jl#L334
-# https://github.com/ACEsuit/ACE1.jl/blob/8ac52d2128241a01d8b9a036f41b1d5106cbeb07/src/rpi/rotations3d.jl#L348
+# CASE 2: 4-correlations, L = 0 (revisited)
+L = 0
+cc = Rot3DCoeffs(L)
+# now we fix an ll = (l1, l2, l3) triple ask for all possible linear combinations 
+# of the tensor product basis   Y[l1, m1] * Y[l2, m2] * Y[l3, m3] * Y[l4, m4]
+# that are invariant under O(3) rotations.
+ll = SA[1,2,2,2,3]
+N = length(ll)
+nn = ones(Int64, N)# for the moment, nn has to be only ones
+@assert length(ll) == length(nn)
+@time coeffs1, MM1 = O3.re_basis(cc, ll)
+nbas_ri1 = size(coeffs1, 1)
 
-# For simplicity, we can just use the SVD to construct a 
-# linearly independent basis.
-
+@time A = f_rand_batch(coeffs1,MM1, 1_000, ll) #this should depend on nn
+rk1 = rank(A, rtol = 1e-12) #does not correspond to what is in our paper
 U, S, V = svd(A)
-coeffs_ind = Diagonal(S[1:rk]) \ (U[:, 1:rk]' * coeffs)
+coeffs_ind1 = Diagonal(S[1:rk1]) \ (U[:, 1:rk1]' * coeffs1)
 
-##
-# now let's re-compute A 
+# Version GD
+@time coeffs_rpi, MM_rpi = MatFmi(nn,ll)
+@show size(coeffs_rpi)
+@time coeffs2, MM2 = ri_basis_new(ll)
 
-ntest = 1_000
-A_ind = zeros(rk, ntest)
-for i = 1:ntest 
-   Rs = [rand_sphere() for _ in 1:4]
-   for q = 1:rk
-      A_ind[q, i] = f(Rs, q; coeffs = coeffs_ind)
-   end
-end
-
-# the rank of this matrix is 3, which is what we expect
-rk_ind = rank(A_ind)  
-# 3 
-
-# And we even scaled the coupling coeffs so that the singular values are 
-# now all close to 1. 
-svdvals(A_ind)
-# 3-element Vector{Float64}:
-#  0.997130965666211
-#  0.8853910366671397
-#  0.8668533817486788
+rk2 = rank(coeffs_rpi,rtol = 1e-12)
+@test rk1 == rk2 #error here
+U, S, V = svd(coeffs_rpi)
+coeffs_ind2 = Diagonal(S[1:rk2]) \ (U[:, 1:rk2]' * coeffs2)
+# multiset_permutations([-1 -1 1 1], 4)
+# sort([[1, 1], [2, 3], [2, 2], [1, 4]])
+@test rank([coeffs_ind1;coeffs_ind2]) == rk1
