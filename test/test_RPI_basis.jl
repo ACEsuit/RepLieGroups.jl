f8fd2be9c6cb8dd5918d4b2b6595a0a3d55f4433
cortner <christophortner0@gmail.com>
2024-11-13 12:26:46 -0800

    add nn dependence to test

--- test/test_RPI_basis.jl
+++ test/test_RPI_basis.jl
@@ -40,33 +40,50 @@ function rand_rot()
    return exp(K - K') 
 end
 
+
 """
 This implements a permutation-invariant and O(3) invariant function of 
 4 (or more) variables. 
 """
-function f(Rs, q::Integer; coeffs=coeffs, MM=MM, ll=ll)
+function f(Rs, q::Integer; coeffs, MM, ll, nn)
+   cheb(r, n) = cos(n * acos(r))
+
+   # q = selection of the basis function 
+   @assert length(coeffs) >= q 
+   @assert length(MM) >= q 
+   # N = number of mm terms to combine ... 
+   #   ... actually no idea what this is???
    N = length(coeffs[1])
+   # @show N 
+   # correlation order 
+   ORD = length(ll) 
+   @assert length(nn) == ORD
+   # @show ORD 
+
    Lmax = maximum(ll)
    real_basis = SphericalHarmonics(Lmax)
    Y = [ eval_cY(real_basis, 𝐫) for 𝐫 in Rs ]
-   A = sum(Y)   # this is a permutation-invariant embedding
-   out = zero(eltype(A))
+   xx = [ (0.1 + norm(𝐫))/(1.1) for 𝐫 in Rs]
+   T = [ cheb(x, n) for x in xx, n in nn ]
+   A = [ sum(Y[j] * T[j, in] for j = 1:length(Rs))   for in = 1:ORD ]
+   out = zero(eltype(A[1]))::ComplexF64 
    for (c, mm) in zip(coeffs[q, :], MM)
-      ii = [SpheriCart.lm2idx(ll[α], mm[α]) for α in 1:N]
-      out += c * prod(A[ii])
+      @assert length(mm) == ORD
+      ii = [ SpheriCart.lm2idx(ll[α], mm[α]) for α in 1:ORD ]
+      out += c * prod(A[α][ii[α]] for α = 1:ORD)
    end
    return real(out)
 end
 
 # we can look for linear independence... 
-function f_rand_batch(coeffs, MM, ntest, ll) 
-   N = length(MM[1])
+function f_rand_batch(coeffs, MM, ntest, ll, nn) 
+   N = 2 * length(ll) # length(MM[1])
    nbas = size(coeffs, 1)
-   A = zeros(nbas, ntest)
+   BB = zeros(nbas, ntest)
    for i = 1:ntest 
-      Rs = [rand_sphere() for _ in 1:N]
+      Rs = [rand_sphere() * rand() for _ in 1:N]
       for q = 1:nbas
-         A[q, i] = f(Rs, q; coeffs = coeffs, MM=MM, ll=ll)
+         BB[q, i] = f(Rs, q; coeffs = coeffs, MM=MM, ll=ll, nn=nn)
       end
    end
    return A
@@ -81,12 +98,12 @@ cc = Rot3DCoeffs(L)
 # that are invariant under O(3) rotations.
 ll = SA[1,2,2,2,3]
 N = length(ll)
-nn = ones(Int64, N)# for the moment, nn has to be only ones
+nn = ones(Int64, N) # for the moment, nn has to be only ones
 @assert length(ll) == length(nn)
 @time coeffs1, MM1 = O3.re_basis(cc, ll)
 nbas_ri1 = size(coeffs1, 1)
 
-@time A = f_rand_batch(coeffs1,MM1, 1_000, ll) #this should depend on nn
+@time A = f_rand_batch(coeffs1,MM1, 1_000, ll, nn)
 rk1 = rank(A, rtol = 1e-12) #does not correspond to what is in our paper
 U, S, V = svd(A)
 coeffs_ind1 = Diagonal(S[1:rk1]) \ (U[:, 1:rk1]' * coeffs1)
@@ -95,6 +112,10 @@ coeffs_ind1 = Diagonal(S[1:rk1]) \ (U[:, 1:rk1]' * coeffs1)
 @time coeffs_rpi, MM_rpi = MatFmi(nn,ll)
 @show size(coeffs_rpi)
 @time coeffs2, MM2 = ri_basis_new(ll)
+
+# @time A_rpi = f_rand_batch(coeffs_rpi, MM_rpi, 1_000, ll, nn) #this should depend on nn
+# rk_rpi = rank(A_rpi, rtol = 1e-12) #does not correspond to what is in our paper
+
 
 rk2 = rank(coeffs_rpi,rtol = 1e-12)
 @test rk1 == rk2 #error here
