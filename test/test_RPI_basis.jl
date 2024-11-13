14f686348ec61ded6e08e375678c1e95a41cf7ae
cortner <christophortner0@gmail.com>
2024-11-13 12:50:32 -0800

    tried to make the test transparent

--- test/test_RPI_basis.jl
+++ test/test_RPI_basis.jl
@@ -46,41 +46,47 @@ This implements a permutation-invariant and O(3) invariant function of
 4 (or more) variables. 
 """
 function f(Rs, q::Integer; coeffs, MM, ll, nn)
-   cheb(r, n) = cos(n * acos(r))
 
    # q = selection of the basis function 
-   @assert length(coeffs) >= q 
-   @assert length(MM) >= q 
-   # N = number of mm terms to combine ... 
-   #   ... actually no idea what this is???
-   N = length(coeffs[1])
-   # @show N 
+   cc = coeffs[q, :]
+
    # correlation order 
    ORD = length(ll) 
    @assert length(nn) == ORD
-   # @show ORD 
+   @assert all( length(mm) == ORD for mm in MM )
 
+   # spherical harmonics 
    Lmax = maximum(ll)
    real_basis = SphericalHarmonics(Lmax)
    Y = [ eval_cY(real_basis, 𝐫) for 𝐫 in Rs ]
-   xx = [ (0.1 + norm(𝐫))/(1.1) for 𝐫 in Rs]
-   T = [ cheb(x, n) for x in xx, n in nn ]
-   A = [ sum(Y[j] * T[j, in] for j = 1:length(Rs))   for in = 1:ORD ]
+
+   # some random radial basis 
+   nmax = maximum(nn) 
+   @assert minimum(nn) >= 1 # radial basis indexing starts at 1 not 0. 
+   _cheb(x) = [ cos( (n-1) * acos(x) ) for n = 1:nmax ]
+   xx = [ (0.1 + norm(𝐫))/(1.1) for 𝐫 in Rs ]
+   T = [ _cheb(x) for x in xx ]
+
+   # pooled tensor product operation -> A[i_lm, n] 
+   A = sum( Y[j] * T[j]' for j = 1:length(Rs) )
+
    out = zero(eltype(A[1]))::ComplexF64 
-   for (c, mm) in zip(coeffs[q, :], MM)
-      @assert length(mm) == ORD
-      ii = [ SpheriCart.lm2idx(ll[α], mm[α]) for α in 1:ORD ]
-      out += c * prod(A[α][ii[α]] for α = 1:ORD)
+   @assert length(cc) == length(MM) 
+   for (c, mm) in zip(cc, MM)
+      # linearize the (l, m) pair into an index i_lm      
+      ii_lm = [ SpheriCart.lm2idx(ll[α], mm[α]) for α in 1:ORD ]
+      out += c * prod( A[ii_lm[α], nn[α]] for α = 1:ORD )
    end
    return real(out)
 end
 
 # we can look for linear independence... 
 function f_rand_batch(coeffs, MM, ntest, ll, nn) 
-   N = 2 * length(ll) # length(MM[1])
+   N = 2 * length(ll) + 1  # length(MM[1]) # length of each group
    nbas = size(coeffs, 1)
    BB = zeros(nbas, ntest)
    for i = 1:ntest 
+      # construct a random set of particles with 𝐫 ∈ ball(radius=1)
       Rs = [rand_sphere() * rand() for _ in 1:N]
       for q = 1:nbas
          BB[q, i] = f(Rs, q; coeffs = coeffs, MM=MM, ll=ll, nn=nn)
@@ -102,9 +108,11 @@ nn = ones(Int64, N) # for the moment, nn has to be only ones
 @assert length(ll) == length(nn)
 @time coeffs1, MM1 = O3.re_basis(cc, ll)
 nbas_ri1 = size(coeffs1, 1)
+rank(coeffs1, rtol = 1e-12)
 
 @time A = f_rand_batch(coeffs1,MM1, 1_000, ll, nn)
 rk1 = rank(A, rtol = 1e-12) #does not correspond to what is in our paper
+
 U, S, V = svd(A)
 coeffs_ind1 = Diagonal(S[1:rk1]) \ (U[:, 1:rk1]' * coeffs1)
 
