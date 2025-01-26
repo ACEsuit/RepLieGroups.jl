c2b37d6a6de2275e797e3bdd81366b75bb19e02c
zhanglw0521 <zhanglw@lsec.cc.ac.cn>
2025-01-26 20:11:01 +0100

    RE_PI function ready to be used and tested
    Still need an SVD or a S2 symmetrization though

--- src/O3.jl
+++ src/O3.jl
@@ -7,7 +7,7 @@ using Combinatorics
 using LinearAlgebra
 using StaticArrays
 
-export re_rpe, rpe_basis_new 
+export re_basis_new, ri_basis_new, ind_corr_s1, ind_corr_s2, MatFmi, ML0, MatFmi2, ri_rpi, re_rpe, rpe_basis_new, re_semi_pi
 
 
 # ------------------------------------------------------- 
@@ -253,6 +253,65 @@ function rpe_basis_new(nn::SVector{N, Int64}, ll::SVector{N, Int64}, L::Int64; f
     U, S, V = svd(gram(FMatrix))
     rk = rank(Diagonal(S); rtol =  1e-12)
     return Diagonal(S[1:rk]) * (U[:, 1:rk]' * UMatrix), MM
-end
+ end
 
-end
+ # ============================ RE_SEMI_PI basis ============================
+
+ # Given nn and ll, generate the Ltot RE basis which are PI for the first N1 variables and also PI for the rest
+function re_semi_pi(nn::SVector{N,Int64},ll::SVector{N,Int64},Ltot::Int64,N1::Int64) where N
+    @assert 0 < N1 < N
+ 
+    ll1 = SA[ll[1:N1]...]
+    ll2 = SA[ll[N1+1:end]...]
+    nn1 = SA[nn[1:N1]...]
+    nn2 = SA[nn[N1+1:end]...]
+ 
+    m_class = m_generate(nn,ll,Ltot)[1]
+    MM = []
+    for i = 1:length(m_class)
+       for j = 1:length(m_class[i])
+          push!(MM, m_class[i][j])
+       end
+    end
+    MM = identity.(MM)
+    MM_dict = Dict(MM[i] => i for i = 1:length(MM))
+    T = Ltot == 0 ? Float64 : SVector{2Ltot+1, Float64}
+    C_re_semi_pi = []
+    counter = 0
+    for L1 in 0:sum(ll1)
+       for L2 in abs(L1-Ltot):minimum([L1+Ltot,sum(ll2)])
+          # global C1, _,_, M1 = re_rpe(nn1,ll1,L1)
+          # global C2, _,_, M2 = re_rpe(nn2,ll2,L2)
+          C1,M1 = rpe_basis_new(nn1,ll1,L1)
+          C2,M2 = rpe_basis_new(nn2,ll2,L2)
+          # global Basis_func = Ltot == 0 ? zeros(Float64, size(C1,1),size(C2,1),length(M1)*length(M2)) : zeros(SVector{2Ltot+1, Float64}, size(C1,1),size(C2,1),length(M1)*length(M2))
+          for i1 in 1:size(C1,1)
+             for i2 in 1:size(C2,1)
+                cc = [ zero(T) for _ = 1:length(MM) ]
+                for (k1,m1) in enumerate(M1)
+                   for (k2,m2) in enumerate(M2)
+                      if abs(sum(m1)+sum(m2))<=Ltot
+                         k = MM_dict[SA[m1...,m2...]] # findfirst(m -> m == SA[m1...,m2...], MM)
+                         # @assert !isnothing(k)
+                         # Basis_func[i1,i2,counter] = Ltot == 0 ? 
+                         #       clebschgordan(L1,sum(m1),L2,sum(m2),Ltot,sum(m1)+sum(m2))*C1[i1,k1][sum(m1)+L1+1]*C2[i2,k2][sum(m2)+L2+1] :
+                         #       clebschgordan(L1,sum(m1),L2,sum(m2),Ltot,sum(m1)+sum(m2))*C1[i1,k1][sum(m1)+L1+1]*C2[i2,k2][sum(m2)+L2+1]*I(2Ltot+1)[sum(m1)+sum(m2)+Ltot+1,:]
+                         cc[k] = Ltot == 0 ? clebschgordan(L1,sum(m1),L2,sum(m2),Ltot,sum(m1)+sum(m2))*C1[i1,k1][sum(m1)+L1+1]*C2[i2,k2][sum(m2)+L2+1] :
+                                             clebschgordan(L1,sum(m1),L2,sum(m2),Ltot,sum(m1)+sum(m2))*C1[i1,k1][sum(m1)+L1+1]*C2[i2,k2][sum(m2)+L2+1]*I(2Ltot+1)[sum(m1)+sum(m2)+Ltot+1,:] 
+                      end
+                   end
+                end
+                push!(C_re_semi_pi, cc)
+                counter += 1
+             end
+          end
+          # BB = reshape(Basis_func,size(C1,1)*size(C2,1),length(M1)*length(M2))
+          # total_rank += rank(gram(BB))
+          # @show size(BB), rank(gram(BB))
+       end
+    end
+    @assert length(C_re_semi_pi) == counter
+    C_re_semi_pi = identity.([C_re_semi_pi[i][j] for i = 1:counter, j = 1:length(MM)])
+ 
+    return C_re_semi_pi, MM
+ end
--- test/recursive_construction.jl
+++ test/recursive_construction.jl
@@ -1,76 +1,12 @@
-using SpheriCart, StaticArrays, LinearAlgebra, RepLieGroups, WignerD,
-      Combinatorics, Rotations
-using RepLieGroups.O3: Rot3DCoeffs, Rot3DCoeffs_real
-O3 = RepLieGroups.O3
-using RepLieGroups: gram, m_generate
+using StaticArrays, LinearAlgebra, RepLieGroups, WignerD, 
+      Rotations
 using Test
-using PartialWaveFunctions
 
-function re_semi_pi(nn::SVector{N,Int64},ll::SVector{N,Int64},Ltot::Int64,N1::Int64) where N
-   @assert 0 < N1 < N
+include("../src/utils/utils_for_tests.jl")
 
-   ll1 = SA[ll[1:N1]...]
-   ll2 = SA[ll[N1+1:end]...]
-   nn1 = SA[nn[1:N1]...]
-   nn2 = SA[nn[N1+1:end]...]
-
-   m_class = m_generate(nn,ll,Ltot)[1]
-   MM = []
-   for i = 1:length(m_class)
-      for j = 1:length(m_class[i])
-         push!(MM, m_class[i][j])
-      end
-   end
-   MM = identity.(MM)
-   MM_dict = Dict(MM[i] => i for i = 1:length(MM))
-   T = Ltot == 0 ? Float64 : SVector{2Ltot+1, Float64}
-   C_re_semi_pi = []
-   counter = 0
-   for L1 in 0:sum(ll1)
-      for L2 in abs(L1-Ltot):minimum([L1+Ltot,sum(ll2)])
-         # global C1, _,_, M1 = re_rpe(nn1,ll1,L1)
-         # global C2, _,_, M2 = re_rpe(nn2,ll2,L2)
-         C1,M1 = rpe_basis_new(nn1,ll1,L1)
-         C2,M2 = rpe_basis_new(nn2,ll2,L2)
-         # global Basis_func = Ltot == 0 ? zeros(Float64, size(C1,1),size(C2,1),length(M1)*length(M2)) : zeros(SVector{2Ltot+1, Float64}, size(C1,1),size(C2,1),length(M1)*length(M2))
-         for i1 in 1:size(C1,1)
-            for i2 in 1:size(C2,1)
-               cc = [ zero(T) for _ = 1:length(MM) ]
-               for (k1,m1) in enumerate(M1)
-                  for (k2,m2) in enumerate(M2)
-                     if abs(sum(m1)+sum(m2))<=Ltot
-                        k = MM_dict[SA[m1...,m2...]] # findfirst(m -> m == SA[m1...,m2...], MM)
-                        # @assert !isnothing(k)
-                        # Basis_func[i1,i2,counter] = Ltot == 0 ? 
-                        #       clebschgordan(L1,sum(m1),L2,sum(m2),Ltot,sum(m1)+sum(m2))*C1[i1,k1][sum(m1)+L1+1]*C2[i2,k2][sum(m2)+L2+1] :
-                        #       clebschgordan(L1,sum(m1),L2,sum(m2),Ltot,sum(m1)+sum(m2))*C1[i1,k1][sum(m1)+L1+1]*C2[i2,k2][sum(m2)+L2+1]*I(2Ltot+1)[sum(m1)+sum(m2)+Ltot+1,:]
-                        cc[k] = Ltot == 0 ? clebschgordan(L1,sum(m1),L2,sum(m2),Ltot,sum(m1)+sum(m2))*C1[i1,k1][sum(m1)+L1+1]*C2[i2,k2][sum(m2)+L2+1] :
-                                            clebschgordan(L1,sum(m1),L2,sum(m2),Ltot,sum(m1)+sum(m2))*C1[i1,k1][sum(m1)+L1+1]*C2[i2,k2][sum(m2)+L2+1]*I(2Ltot+1)[sum(m1)+sum(m2)+Ltot+1,:] 
-                     end
-                  end
-               end
-               push!(C_re_semi_pi, cc)
-               counter += 1
-            end
-         end
-         # BB = reshape(Basis_func,size(C1,1)*size(C2,1),length(M1)*length(M2))
-         # BB = BB[:,1:length(MM)]
-         # @assert length(MM) == counter-1
-
-         # total_rank += rank(gram(BB))
-         # @show size(BB), rank(gram(BB))
-      end
-   end
-   @assert length(C_re_semi_pi) == counter
-   C_re_semi_pi_final = identity.([C_re_semi_pi[i][j] for i = 1:counter, j = 1:length(MM)])
-
-   return C_re_semi_pi_final, MM
-end
-
-
-for i = 1:200
-   ll = SA[rand(0:2, 6)...]
-   nn = SA[rand(0:2, 6)...]
+for ntest = 1:200
+   ll = SA[rand(0:1, 6)...]
+   nn = SA[rand(1:2, 6)...]
    # nn = SA[1,1,1,1,2,2]
    # ll = SA[1,2,3,1,2,3]
    N1 = rand(1:length(ll)-1) # random partition
@@ -79,12 +15,81 @@ for i = 1:200
    if isodd(sum(ll)+Ltot); continue; end
    # if isodd(sum(ll)+Ltot); Ltot += 1; end
    t1 = @elapsed C_re_semi_pi, MM = re_semi_pi(nn,ll,Ltot,N1)
-   t2 = @elapsed C_re,C_rpe,_,M = re_rpe(nn,ll,Ltot)
-   @show t1, t2
+   t2 = @elapsed C_re,_,_,M = re_rpe(nn,ll,Ltot)
+   t3 = @elapsed C_rpe,M = rpe_basis_new(nn,ll,Ltot)
+   @show t1, t2, t3
+   
    if rank(gram(C_rpe)) > 0
-      @show rank(gram(C_re))
-      @show rank(gram(C_re_semi_pi))
-      @show rank(gram(C_rpe))
+      # @info("Test that re_semi_pi span a set with rank ranging between RE and RPE")
       @test rank(gram(C_rpe)) <= rank(gram(C_re_semi_pi)) <= rank(gram(C_re))
+
+      # @info("Testing the equivariance of the old RPE basis")
+      Rs = rand_config(length(ll))
+      θ = rand(3) * 2pi
+      Q = RotZYZ(θ...)
+      D = transpose(WignerD.wignerD(Ltot, θ...)) 
+      QRs = [Q*Rs[i] for i in 1:length(Rs)]
+      fRs1 = eval_basis(Rs; coeffs = C_re_semi_pi, MM = MM, ll = ll, nn = nn)
+      fRs1Q = eval_basis(QRs; coeffs = C_re_semi_pi, MM = MM, ll = ll, nn = nn)
+      Ltot == 0 ? (@test norm(fRs1 - fRs1Q) < 1e-14) : (@test norm(fRs1 - Ref(D) .* fRs1Q) < 1e-14)
+
+      # @info("Test that re_semi_pi span the same space as RPE")
+      # Do the rand batch on the same set of points
+      ntest = 1000
+      ORD = length(ll) # length of each group 
+      BB1 = complex.(zeros(typeof(C_re_semi_pi[1]), size(C_re_semi_pi, 1), ntest))
+      BB2 = complex.(zeros(typeof(C_rpe[1]), size(C_rpe, 1), ntest))
+      for i = 1:ntest 
+         # construct a random set of particles with 𝐫 ∈ ball(radius=1)
+         Rs = [ rand_ball() for _ in 1:ORD ]
+         BB1[:, i] = eval_basis(Rs; coeffs=C_re_semi_pi, MM=MM, ll=ll, nn=nn) 
+         BB2[:, i] = eval_basis(Rs; coeffs=C_rpe, MM=MM, ll=ll, nn=nn) 
+      end
+      @test rank(gram(C_re_semi_pi)) == rank(gram(BB1); rtol=1e-11) == rank(gram([BB1;BB2]); rtol=1e-11) >= rank(gram(BB2); rtol=1e-11) == rank(gram(C_rpe))
+   end
+end
+
+llset = [SA[1,1,1,1], SA[1,1,2,2], SA[1,2,2,2], SA[1,1,2,2,2], SA[1,1,2,2,3], SA[1,1,1,2,2,3] ]
+nnset = [SA[1,1,2,2], SA[1,1,2,3], SA[1,3,1,2], SA[1,2,1,2,2], SA[1,1,1,1,1], SA[2,2,1,1,2,1] ]
+Partition = [2,2,1,2,2,3]
+
+for ntest = 1:200
+   k = rand(1:length(llset))
+   nn = nnset[k]
+   ll = llset[k]
+   N1 = Partition[k]
+   
+   Ltot = iseven(sum(ll)) ? rand(0:2:4) : rand(1:2:3)
+   t1 = @elapsed C_re_semi_pi, MM = re_semi_pi(nn,ll,Ltot,N1)
+   t2 = @elapsed C_rpe,M = rpe_basis_new(nn,ll,Ltot)
+   @show t1, t2
+   
+   if rank(gram(C_rpe)) > 0
+      # @info("Test that re_semi_pi span a set with rank ranging between RE and RPE")
+      @test rank(gram(C_rpe)) == rank(gram(C_re_semi_pi))
+
+      # @info("Testing the equivariance of the old RPE basis")
+      Rs = rand_config(length(ll))
+      θ = rand(3) * 2pi
+      Q = RotZYZ(θ...)
+      D = transpose(WignerD.wignerD(Ltot, θ...)) 
+      QRs = [Q*Rs[i] for i in 1:length(Rs)]
+      fRs1 = eval_basis(Rs; coeffs = C_re_semi_pi, MM = MM, ll = ll, nn = nn)
+      fRs1Q = eval_basis(QRs; coeffs = C_re_semi_pi, MM = MM, ll = ll, nn = nn)
+      Ltot == 0 ? (@test norm(fRs1 - fRs1Q) < 1e-14) : (@test norm(fRs1 - Ref(D) .* fRs1Q) < 1e-14)
+
+      # @info("Test that re_semi_pi span the same space as RPE")
+      # Do the rand batch on the same set of points
+      ntest = 1000
+      ORD = length(ll) # length of each group 
+      BB1 = complex.(zeros(typeof(C_re_semi_pi[1]), size(C_re_semi_pi, 1), ntest))
+      BB2 = complex.(zeros(typeof(C_rpe[1]), size(C_rpe, 1), ntest))
+      for i = 1:ntest 
+         # construct a random set of particles with 𝐫 ∈ ball(radius=1)
+         Rs = [ rand_ball() for _ in 1:ORD ]
+         BB1[:, i] = eval_basis(Rs; coeffs=C_re_semi_pi, MM=MM, ll=ll, nn=nn) 
+         BB2[:, i] = eval_basis(Rs; coeffs=C_rpe, MM=MM, ll=ll, nn=nn) 
+      end
+      @test rank(gram(C_re_semi_pi)) == rank(gram(BB1); rtol=1e-11) == rank(gram([BB1;BB2]); rtol=1e-11) == rank(gram(BB2); rtol=1e-11) == rank(gram(C_rpe))
    end
 end
