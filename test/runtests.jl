f7d0685bc4e6f551a621a24a0aac45ff48fcd1af
cortner <christophortner0@gmail.com>
2025-04-14 15:30:26 -0700

    final cleanup for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,11 +9,7 @@ isdefined(Main, :___UTILS_FOR_TESTS___) || include("utils/utils_for_tests.jl")
 
     @testset "SYYVector" begin include("test_yyvector.jl"); end
     
-    @testset "CGcoef vs PartialWaveFunctions" begin include("test_cg_vs_partialwavefunctions.jl"); end
-    
-    # @testset "O3-ClebschGordan" begin include("test_obsolete_cg.jl"); end
-    
-    # @testset "O3" begin include("test_obsolete_o3.jl"); end
+    @testset "Clebsch Gordan Coeffs" begin include("test_clebschgordans.jl"); end
 
     # @testset "O3 new" begin include("new_rpe_test.jl"); end
 
